b1dafe9f04289e57fd529a09cbebdbcf0456584e
Ukraine1449 <roma.bystriakov@gmail.com>
2021-10-30 19:33:59 -0400

    Trying to figure out how to fucking save 2 coords. Slightly drunk, cant fucking think of it. god fucking damn it.

--- src/main/java/com/github/ukraine1449/claimplugin/Events/rightClickEvent.java
+++ src/main/java/com/github/ukraine1449/claimplugin/Events/rightClickEvent.java
@@ -1,5 +1,6 @@
 package com.github.ukraine1449.claimplugin.Events;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,7 +16,9 @@ public class rightClickEvent implements Listener {
         Player player = event.getPlayer();
         Action a = event.getAction();
         if ((a == Action.PHYSICAL) || (event.getItem().getType() == Material.GOLDEN_SHOVEL)){
+            if(event.getItem().getItemMeta().getDisplayName().equals(ChatColor.GOLD + "Claim tool")){
 
+            }
         }
     }
 
