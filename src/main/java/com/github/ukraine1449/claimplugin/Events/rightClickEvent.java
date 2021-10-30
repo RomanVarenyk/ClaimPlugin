8b3450744997fb532ef9dce9ac409ffb58a6c1af
Ukraine1449 <roma.bystriakov@gmail.com>
2021-10-30 17:52:47 -0400

    Create right click event

---
+++ src/main/java/com/github/ukraine1449/claimplugin/Events/rightClickEvent.java
@@ -0,0 +1,22 @@
+package com.github.ukraine1449.claimplugin.Events;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class rightClickEvent implements Listener {
+
+    @EventHandler
+    public void rightClickEvent(PlayerInteractEvent event) throws Exception {
+        Player player = event.getPlayer();
+        Action a = event.getAction();
+        if ((a == Action.PHYSICAL) || (event.getItem().getType() == Material.GOLDEN_SHOVEL)){
+
+        }
+    }
+
+}
