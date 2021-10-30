e2ad8996f53277c39eb4590eed6debfb5105f76f
Ukraine1449 <roma.bystriakov@gmail.com>
2021-10-30 17:32:20 -0400

    Added block place event

--- src/main/java/com/github/ukraine1449/claimplugin/Events/BlockInteractEvent.java
+++ src/main/java/com/github/ukraine1449/claimplugin/Events/BlockInteractEvent.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -36,6 +36,23 @@ ClaimPlugin plugin;
             event.setCancelled(true);
             player.sendMessage(ChatColor.RED + "This block is claimed.");
         }
+    }    @EventHandler
+    public void blockPlaceEvent(BlockPlaceEvent event) throws Exception {
+        int xpos = event.getBlock().getX();
+        int zpos = event.getBlock().getZ();
+        Player player = event.getPlayer();
+        ArrayList<Integer> coordsOrg = plugin.selectCD(player.getWorld().getName(), player.getLocation().getChunk().toString());
+        if(!plugin.cache.contains(event.getBlock().getLocation())){
+            if(xpos <= Math.max(coordsOrg.get(0), coordsOrg.get(1)) && xpos >= Math.min(coordsOrg.get(0), coordsOrg.get(1))){
+                if(zpos <= Math.max(coordsOrg.get(2), coordsOrg.get(3)) && zpos >= Math.min(coordsOrg.get(2), coordsOrg.get(3))){
+                    plugin.cache.add(event.getBlock().getLocation());
+                    event.setCancelled(true);
+                    player.sendMessage(ChatColor.RED + "This block is claimed.");
+                }
+            }
+        }else{
+            event.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "This block is claimed.");
+        }
     }
-
 }
