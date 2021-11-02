f5b793b0fddc2ccab156482ec051d168f0abeb33
Ukraine1449 <roma.bystriakov@gmail.com>
2021-11-01 21:02:44 -0400

    Added add claim max to players

---
+++ src/main/java/com/github/ukraine1449/claimplugin/Commands/addClaimsMax.java
@@ -0,0 +1,34 @@
+package com.github.ukraine1449.claimplugin.Commands;
+
+import com.github.ukraine1449.claimplugin.ClaimPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class addClaimsMax implements CommandExecutor {
+    public addClaimsMax(ClaimPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    ClaimPlugin plugin;
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            Player target = Bukkit.getPlayerExact(args[0]);
+            int newLimit = Integer.parseInt(args[1]);
+            try {
+                plugin.postUD(player.getUniqueId().toString(), 1, newLimit, 1);
+            } catch (Exception e) {
+                e.printStackTrace();
+                plugin.errorOccur(player);
+            }
+        }
+
+        return false;
+    }
+}
